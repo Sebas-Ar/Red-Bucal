2c15adba2fe0408496f8d644828b14d4a78bde73
Sebas <juan_ariasd@hotmail.com>
2020-04-07 00:24:55 -0500

    test pay

--- components/usuario/items/BillingUser.js
+++ components/usuario/items/BillingUser.js
@@ -12,27 +12,32 @@ const BillingUser = (props) => {
 
     const pay = async () => {
         /* my IP : 192.168.1.11 */
-        const url = 'https://sandbox.paguelofacil.com/rest/ccprocessing'
+        const url = ''
         const result = await axios({
-            method: 'post',
-            url,
-            headers: { 'cache-control': 'no-cache', 'Content-Type': 'application/x-www-form-urlencoded' },
-            form: {
-                CCLW: 'D17B05A095489D1176560B4666A283454185F353F401D0201CC5C16F92535DF6B1DEBA18E79442CC0D6F75FD024207680AFBDFD6CF015478BF30CBEF9160A08D',
-                txType: 'SALE',
-                CMTN: '0',
-                CDSC: 'test de compra',
-                CCNum: '4059310181757001',
-                ExpMonth: '03',
-                ExpYear: '21',
-                CVV2: '266',
-                Name: 'MANUEL',
-                LastName: 'ARIAS',
-                Email: 'sebas_ariasd@hotmail.com',
-                Address: 'colombia',
-                Tel: '3203709957',
-                Ip: '157.230.6.176',
-                SecretHash: '0337f80c8a19dee560a5d3dc291c472c4e9be3e35becbff2847537c9f5e44989'
+            method: 'POST',
+            url: 'https://sandbox.paguelofacil.com/rest/processTx/AUTH_CAPTURE',
+            headers: {
+                "authorization": 'WT5hTaUcpa4J3h4AmrZa2EXXJs8boUVa|DIRd852djHbq2j5Fca5VDUkDbExTBCVf',
+                "content-type": 'application/json'
+            },
+            body: {
+                cclw: 'D17B05A095489D1176560B4666A283454185F353F401D0201CC5C16F92535DF6B1DEBA18E79442CC0D6F75FD024207680AFBDFD6CF015478BF30CBEF9160A08D ',
+                amount: 3.50,//El monto o valor total de la transacción a realizar. NO PONER
+                taxAmount: 1.0,
+                email: 'alambrito@correo.com',//String MaxLength:100 Email del
+                phone: '60201236',//Numeric MaxLength:16 Teléfono del Tarjeta habiente,
+                address: 'testing new address',//String MaxLength:100 Dirección del Tarjeta,
+                concept: 'Nro-Order-523',//MaxLength:150 ;Es la descripción o el motivo de la transacción en proceso
+                description: 'Nro-Order-523',//MaxLength:150 ;Es la descripción o el motivo de la transacción en proceso
+                cardInformation: {
+                    cardNumber: '4059310181757001',
+                    expMonth: "12",//Mes de expiración de la tarjeta, siempre 2 dígitos
+                    expYear: "32",//Numeric Ej.:02 Año de expiración de la tarjeta.
+                    cvv: "003",//Código de Seguridad de la tarjeta Numeric MaxLength:3
+                    firstName: 'alam',//String MaxLength:25 Nombre del tarjeta habiente
+                    lastName: 'brito',//String MaxLength:25 Apellido del Tarjeta habiente
+                    cardType: 'VISA'
+                }
             }
         });
         /* axios.post(url,
