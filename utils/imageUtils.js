4a9e4c75de8bae9fe2744683270459b9c5824611
OCTAVIO\ourquiola <ourquiola@gmail.com>
2023-12-06 23:05:02 -0500

    Actualizacoión de Carnet

--- utils/imageUtils.js
+++ utils/imageUtils.js
@@ -10,15 +10,15 @@ export async function generateImage(name, identification, start, end, plan) {
     registerFont('./public/fonts/David_Libre/DavidLibre-Regular.ttf', { family: 'David Libre', weight: 'bold' });
 
     // Agregar texto personalizado al lienzo
-    const xCoord = 30;
-    const yCoord = 320;
-    const fontSizeName = 24;
-    const fontSize = 16;
-    const color = 'rgb(252,223,133)';
+    const xCoord = 50;
+    const yCoord = 210;
+    const fontSizeName = 18;
+    const fontSize = 14;
+    const color = 'white';
     const options = { month: 'long' };
     const locale = 'es-ES';
     // Cargar la plantilla de imagen
-    const templatePath = './public/Template.png'; // Ruta de la plantilla de imagen
+    const templatePath = './public/Template_V3.png'; // Ruta de la plantilla de imagen
     const templateImage = await loadImage(templatePath);
 
     // Crear un lienzo (canvas) para dibujar la imagen
@@ -28,12 +28,12 @@ export async function generateImage(name, identification, start, end, plan) {
     const dateStart = new Date(start);
     let monthStart = dateStart.toLocaleString(locale, options);
     monthStart = monthStart.charAt(0).toUpperCase() + monthStart.slice(1).toLowerCase();
-    const vigenciaStart = `${dateStart.getDate()} | ${monthStart} | ${dateStart.getFullYear()}`;
+    const vigenciaStart = `${dateStart.getDate()} ${monthStart} ${dateStart.getFullYear()}`;
 
     const dateEnd = new Date(end);
     let monthEnd = dateEnd.toLocaleString(locale, options);
     monthEnd = monthEnd.charAt(0).toUpperCase() + monthEnd.slice(1).toLowerCase();
-    const vigenciaEnd = `${dateEnd.getDate()} | ${monthEnd} | ${dateEnd.getFullYear()}`;
+    const vigenciaEnd = `${dateEnd.getDate()} ${monthEnd} ${dateEnd.getFullYear()}`;
 
     // Dibujar la plantilla de imagen en el lienzo
     context.drawImage(templateImage, 0, 0);
@@ -41,13 +41,13 @@ export async function generateImage(name, identification, start, end, plan) {
     // Agregar texto personalizado al lienzo
     context.font = `${fontSizeName}px Lato`;
     context.fillStyle = color;
-    context.fillText(`${name}`, xCoord, yCoord - 60);
+    context.fillText(`${name}`, xCoord, yCoord - 40);
 
     context.font = `${fontSize}px "David Libre"`;
     context.fillStyle = color;
-    context.fillText(`Cédula: ${identification}`, xCoord, yCoord - 30);
+    context.fillText(`Cédula: ${identification}`, xCoord, yCoord - 20);
 
-    if (plan == "false") context.fillText(`Vigencia:  desde: ${vigenciaStart}   hasta: ${vigenciaEnd}`, xCoord, yCoord);
+    if (plan == "false") context.fillText(`Vigencia: ${vigenciaStart} > ${vigenciaEnd}`, xCoord, yCoord);
     else context.fillText(`Vigencia: ${vigenciaStart}`, xCoord, yCoord);
 
     // Generar los bytes de la imagen en formato PNG
